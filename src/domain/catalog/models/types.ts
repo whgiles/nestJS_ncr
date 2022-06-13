e3c94d7714688d03de38edb4f40feb7139923d59
hg185078 <hg185078@ncr.com>
2022-06-13 16:36:00 -0400

    domain/catalog module added

---
+++ src/domain/catalog/models/types.ts
@@ -0,0 +1,42 @@
+export interface IItem {
+  version: number;
+
+  itemId: {
+    itemCode: string;
+  };
+
+  packageIdentifiers: {
+    type: string;
+    value: string;
+  };
+
+  shortDescription: {
+    locale: string;
+    value: string;
+  };
+
+  longDescription: {
+    locale: string;
+    value: string;
+  };
+
+  status: any;
+
+  merchandiseCategory: {
+    nodeld: string;
+  };
+
+  departmentId: string;
+  familyCode: string;
+  manufacturerCode: string;
+  nonMerchandise: boolean;
+
+  externalIdentifiers: {
+    type: any;
+    value: string;
+  };
+
+  posNumber: string;
+
+  referenceId: string;
+}
