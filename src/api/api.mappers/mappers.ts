203da709f5e3cbab65ce627ef24b17f694cdb986
hg185078 <hg185078@ncr.com>
2022-06-17 20:06:37 -0400

    refactored the mappers

--- src/api/api.mappers/mappers.ts
+++ src/api/api.mappers/mappers.ts
@@ -5,6 +5,7 @@ import {
   INodeIdData,
 } from 'src/domain/domain.catalog/models/types';
 import { CreateItemDto } from '../api.catalog/dtos/request/createItem.request.dto';
+import { MultiLanguageDataDto } from '../api.catalog/types/types';
 
 export class ApiMapper {
   dtoToIitem(dto: CreateItemDto): IItem {
@@ -13,27 +14,37 @@ export class ApiMapper {
     item.status = dto.status;
     item.departmentId = dto.departmentId;
     item.nonMerchandise = dto.nonMerchandise;
-
-    const nodeIdData = new INodeIdData();
-    nodeIdData.nodeId = dto.merchandiseCategory.nodeId;
-    item.merchandiseCategory = nodeIdData;
-
-    const multiLanguageData = new IMultiLanguageData();
-    let localizedTextDataArray: ILocalizedTextData[] = [];
-
-    dto.shortDescription.values.forEach((v) =>
-      localizedTextDataArray.push(this.localDtoToIlocal(v)),
+    item.shortDescription = this.multiLanguageDataDto_TO_IMultiLanguageData(
+      dto.shortDescription,
     );
-    multiLanguageData.values = localizedTextDataArray;
-    item.shortDescription = multiLanguageData;
+    item.merchandiseCategory = this.nodeDto_TO_INode(dto.merchandiseCategory);
 
     return item;
   }
 
-  localDtoToIlocal(dto: any) {
+  private localDto_TO_Ilocal(dto: any): ILocalizedTextData {
     const iLocalizedTextData = new ILocalizedTextData();
     iLocalizedTextData.locale = dto.locale;
     iLocalizedTextData.value = dto.value;
     return iLocalizedTextData;
+  }
+
+  private nodeDto_TO_INode(dto: any): INodeIdData {
+    const iNodeIdData = new INodeIdData();
+    iNodeIdData.nodeId = dto.nodeId;
+    return iNodeIdData;
+  }
+
+  private multiLanguageDataDto_TO_IMultiLanguageData(
+    dto: MultiLanguageDataDto,
+  ): IMultiLanguageData {
+    const iMultiLanguageData = new IMultiLanguageData();
+    const iLocalizedTextDataArray: ILocalizedTextData[] = [];
+
+    dto.values.forEach((i) =>
+      iLocalizedTextDataArray.push(this.localDto_TO_Ilocal(i)),
+    );
+    iMultiLanguageData.values = iLocalizedTextDataArray;
+    return iMultiLanguageData;
   }
 }
--- src/utils/utils.mappers/mappers.ts
+++ src/utils/utils.mappers/mappers.ts
@@ -9,25 +9,41 @@ export class UtilMapper {
   constructor() {}
 
   JsonToBslItemResponse(json: any) {
-    let bslItemResponse = new BSLItemResponse(),
-      multiLanguageData = new BslMultiLanguageData(),
-      localizedTextData = new BslLocalizedTextData(),
-      nodeIdData = new BslNodeIdData();
-
-    localizedTextData.locale = json.shortDescription.locale;
-
-    localizedTextData.value = json.shortDescription.value;
-    multiLanguageData.values = [localizedTextData];
-    nodeIdData.nodeId = json.merchandiseCategory.nodeId;
+    let bslItemResponse = new BSLItemResponse();
     bslItemResponse.itemId = json.itemId.itemCode;
     bslItemResponse.version = json.version;
-    bslItemResponse.shortDescription = multiLanguageData;
-    bslItemResponse.merchandiseCategory = nodeIdData;
+    bslItemResponse.shortDescription =
+      this.multiLanguageDataJSON_TO_bslMultiLanguageData(json.shortDescription);
+    bslItemResponse.merchandiseCategory = this.nodeJSON_TO_bslNode(
+      json.merchandiseCategory,
+    );
     bslItemResponse.status = json.status;
     bslItemResponse.departmentId = json.departmentId;
     bslItemResponse.nonMerchandise = json.nonMerchandise;
-    bslItemResponse.itemId = json.itemId.itemCode;
 
     return bslItemResponse;
+  }
+  private localJSON_TO_bsllocal(dto: any): BslLocalizedTextData {
+    const localizedTextDataJSON = new BslLocalizedTextData();
+    localizedTextDataJSON.locale = dto.locale;
+    localizedTextDataJSON.value = dto.value;
+    return localizedTextDataJSON;
+  }
+
+  private nodeJSON_TO_bslNode(dto: any): BslNodeIdData {
+    const nodeIdData = new BslNodeIdData();
+    nodeIdData.nodeId = dto.nodeId;
+    return nodeIdData;
+  }
+
+  private multiLanguageDataJSON_TO_bslMultiLanguageData(
+    dto: any,
+  ): BslMultiLanguageData {
+    const multiLanguageData = new BslMultiLanguageData();
+    const localizedTextDataArray: BslLocalizedTextData[] = [];
+    localizedTextDataArray.push(this.localJSON_TO_bsllocal(dto.value));
+
+    multiLanguageData.values = localizedTextDataArray;
+    return multiLanguageData;
   }
 }
